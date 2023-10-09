6a2d297c4fc25fb97dedecddc1c9dfe4bc67739c
lvdianzhao <lv_workwith@163.com>
2023-10-09 17:23:15 +0800

    1

--- src/main/java/com/example/demo/web/controller/Helloword.java
+++ src/main/java/com/example/demo/web/controller/Helloword.java
@@ -1,13 +1,39 @@
 package com.example.demo.web.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.web.entity.PracticeEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
-@RequestMapping("/helloword")
+@RequestMapping("hello")
 public class Helloword {
+    Logger log = LoggerFactory.getLogger(getClass());
+    @GetMapping
     public String hello() {
         return "Hello World";
     }
+    @PostMapping
+
+    public PracticeEntity addId(@RequestBody PracticeEntity practiceEntity){
+        return practiceEntity;
+    }
+    @GetMapping("/{id}")
+    @ResponseBody
+    public String getById(@PathVariable int id){
+        System.out.println("get id:"+id);
+        return "get id:"+id;
+    }
+
+    @GetMapping("/test")
+    public void test(){
+        log.trace("我是 trace");
+        log.debug("我是 debug");
+        log.info("我是 info");
+        log.warn("我是 warn");
+        log.error("我是 error");
+    }
+
 }
