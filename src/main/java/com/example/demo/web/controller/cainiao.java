18aff8d1235cee4d5268f13b5cde1246b31c40e4
lvdianzhao <lv_workwith@163.com>
2023-10-10 17:33:58 +0800

    1

---
+++ src/main/java/com/example/demo/web/controller/cainiao.java
@@ -0,0 +1,22 @@
+package com.example.demo.web.controller;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+@Component
+public class cainiao {
+    @PostConstruct
+    public static void test() {
+        Scanner scan = new Scanner(System.in);
+        // 从键盘接收数据
+
+        // nextLine方式接收字符串
+        System.out.println("nextLine方式接收：");
+        // 判断是否还有输入
+        if (scan.hasNextLine()) {
+            String str2 = scan.nextLine();
+            System.out.println("输入的数据为：" + str2);
+        }
+        scan.close();
+    }
+}
